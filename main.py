fc01cd1be3b976a3075f1a9a889a8e8a4bc7d71b
Jugaad <93738828+jug-3866@users.noreply.github.com>
2023-06-13 14:50:11 -0700

    reset brightness bump

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def set_laptop_brightness(brightness):
     brightness = int((brightness * 100)+brightness_bump)
     wmi.WMI(namespace='wmi').WmiMonitorBrightnessMethods()[0].WmiSetBrightness(brightness, 0)
 
-brightness_bump = 15 #Adjust to bump up the brightness or lower if not working
+brightness_bump = 0 #Adjust to bump up the brightness or lower if not working
 camera_index = 0  #select camera
-exposure_value = -3  #Use in conjuction with brightness_bump 
+exposure_value = -4  #Use in conjuction with brightness_bump 
 interval = 1  # Interval between capturing images excluiding camera initialization time
 
 start_time = time.time()
