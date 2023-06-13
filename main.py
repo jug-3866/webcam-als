5ba253fc9c9b9098876fd212a6a1d6949b090ef1
Jugaad <93738828+jug-3866@users.noreply.github.com>
2023-06-13 14:47:41 -0700

    fixed spacing. will not exit if camera read fails

--- main.py
+++ main.py
@@ -7,19 +7,13 @@ def set_camera_exposure(camera_index, exposure_value):
     camera.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
     camera.set(cv2.CAP_PROP_EXPOSURE, exposure_value)
     return camera
-
 def calculate_brightness(image):
     average_brightness = cv2.mean(image)[0] / 255.0
     return average_brightness
-
-
-
 def set_laptop_brightness(brightness):
     brightness = int((brightness * 100)+brightness_bump)
     wmi.WMI(namespace='wmi').WmiMonitorBrightnessMethods()[0].WmiSetBrightness(brightness, 0)
 
-
-# Usage example
 brightness_bump = 15 #Adjust to bump up the brightness or lower if not working
 camera_index = 0  #select camera
 exposure_value = -3  #Use in conjuction with brightness_bump 
@@ -35,8 +29,6 @@ while True:
         print("Relative brightness:", brightness)
         set_laptop_brightness(brightness)
 
-
     camera.set(cv2.CAP_PROP_EXPOSURE, exposure_value)
     time.sleep(interval)
 
-cv2.destroyAllWindows()
